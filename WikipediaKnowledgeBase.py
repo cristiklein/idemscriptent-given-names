cad750df0c1e7bc076a8806794ac90332220b132
Cristian Klein <cristiklein@gmail.com>
2015-12-07 01:19:45 +0100

    Parse WP pages to find given names

--- WikipediaKnowledgeBase.py
+++ WikipediaKnowledgeBase.py
@@ -1,6 +1,7 @@
 'This module contains the WikipediaKnowledgeBase class'
 
 import logging
+import requests
 import time
 import unittest
 
@@ -55,11 +56,65 @@ class LoggingTest(unittest.TestCase):
 class WikipediaKnowledgeBase:
 	'Taps into Wikipedia to retrieve trivia about given names.'
 
+	ARTICLE_URL_TEMPLATE='https://en.wikipedia.org/w/api.php?action=parse&format=json&redirects=1&page={0}'
+	START_ARTICLE='List of most popular given names'
+
 	DEFAULT_LANGUAGES = 'de en fr pl ro sv'.split()
 	GENDERS = 'male female'.split()
 
 	def __init__(self, languages = DEFAULT_LANGUAGES):
 		self.languages = languages
+		self.session = requests.session()
+
+		self.namesCache = None
+
+	@logStartAndEnd
+	def getNamesCache(self):
+		if self.namesCache:
+			return self.namesCache
+
+		namesCache = { 'male': [ ], 'female': [ ], 'unknown': [ ] }
+		givenNamesPage = self.session.get(self.ARTICLE_URL_TEMPLATE.format(self.START_ARTICLE)).json()
+		for link in givenNamesPage['parse']['links']:
+			possibleGivenNameArticle = link['*']
+			self.checkArticleForGivenName(possibleGivenNameArticle, namesCache)
+
+		# Ensures atomicity
+		self.namesCache = namesCache
+
+	@logStartAndEnd
+	def checkArticleForGivenName(self, possibleGivenNameArticle, namesCache):
+		# TODO: URL safe
+		givenNamePage = self.session.get(self.ARTICLE_URL_TEMPLATE.format(possibleGivenNameArticle)).json()
+		if 'error' in givenNamePage:
+			logger.debug('Got error page for %s', possibleGivenNameArticle)
+			return
+		title = givenNamePage['parse']['title']
+		text = givenNamePage['parse']['text']['*']
+		lowerCaseText = text.lower()
+
+		if 'given name' in title or 'given name' in text:
+			givenName = title.partition(' ')[0]
+			whereMasculine = lowerCaseText.find('masculine')
+			whereFeminine = lowerCaseText.find('feminine')
+			if whereMasculine == -1:
+				whereMasculine = lowerCaseText.find(' male') # avoid matching 'fe*male*'
+				if whereMasculine == -1:
+					whereMasculine = len(text)
+			if whereFeminine == -1:
+				whereFeminine = lowerCaseText.find('female')
+				if whereFeminine == -1:
+					whereFeminine = len(text)
+
+			if whereMasculine < whereFeminine:
+				logger.debug('Found male given name %s', givenName)
+				namesCache['male'].append(givenName)
+			elif whereFeminine < whereMasculine:
+				logger.debug('Found female given name %s', givenName)
+				namesCache['female'].append(givenName)
+			else:
+				logger.debug('Found unknown given name %s', givenName)
+				namesCache['unknown'].append(givenName)
 
 	@logStartAndEnd
 	def retrieveGivenNames(self, gender):
@@ -70,11 +125,8 @@ class WikipediaKnowledgeBase:
 			:param gender 'male' or 'female'
 		'''
 
-		# TODO
-		if gender == 'male':
-			return [ 'Ahmed', 'Mohammed', 'Mahamad', 'David', 'Davit' ]
-		elif gender == 'female':
-			return [ 'Fatima', 'Fatime', 'Linda' ]
+		if gender in [ 'male', 'female' ]:
+			return self.getNamesCache()[gender]
 		else:
 			raise ValueError('Gender must be male or female')
 
