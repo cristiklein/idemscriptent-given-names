b5524e8b9e149e7b2fac4444d1a613a6ae85252b
Cristian Klein <cristiklein@gmail.com>
2015-12-09 21:20:47 +0100

    Started on a completly new approach

--- find-idemscriptent-given-names.py
+++ find-idemscriptent-given-names.py
@@ -2,19 +2,9 @@
 
 import logging
 
-from WikipediaKnowledgeBase import WikipediaKnowledgeBase
-
 if __name__ == '__main__':
 	logging.basicConfig(
 		level = logging.DEBUG,
 		format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 	)
 
-	kb = WikipediaKnowledgeBase()
-
-	for gender in kb.GENDERS:
-		givenNames = kb.retrieveGivenNames(gender)
-		for givenName in givenNames:
-			alternateSpellings = kb.retrieveAlternateSpellings(givenName)
-			if not alternateSpellings:
-				print(givenName)
