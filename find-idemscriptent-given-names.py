52dc6c184aa2cdf46d3e360ae4824e6174289a19
Cristian Klein <cristiklein@gmail.com>
2015-12-13 23:09:40 +0100

    Cleanup; ported to Python3

--- find-idemscriptent-given-names.py
+++ find-idemscriptent-given-names.py
@@ -1,19 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 from collections import defaultdict
 from unidecode import unidecode
 from glob import glob
-import logging
-
-from reduceName import reduceName
 
 if __name__ == '__main__':
-	logging.basicConfig(
-		level = logging.DEBUG,
-		format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-	)
-
 	genderToNameToLanguages = { 'm': defaultdict(list), 'f': defaultdict(list) }
 	for fileName in glob('names-*.txt'):
 		fileNameWithoutExt, _ = fileName.split('.')
@@ -22,16 +13,16 @@ if __name__ == '__main__':
 		with open(fileName) as f:
 			for line in f.readlines():
 				for name in line.split('/'):
-					name = name.decode('utf8').strip().capitalize()
+					name = name.strip().capitalize()
 					# eliminate diacritic marks, e.g., é becomes e
 					name = unidecode(name)
 					genderToNameToLanguages[gender][name].append(language)
 
 	for gender in genderToNameToLanguages:
-		print '===', gender, '==='
-		for name, languages in sorted(genderToNameToLanguages[gender].iteritems(),
+		print('===', gender, '===')
+		for name, languages in sorted(genderToNameToLanguages[gender].items(),
 			key = lambda nameLanguage: -len(nameLanguage[1])):
 
 			languages = sorted(languages)
 			if len(languages) > 2:
-				print "{0}: {1}".format(name, ' '.join(languages))
+				print("{0}: {1}".format(name, ' '.join(languages)))
